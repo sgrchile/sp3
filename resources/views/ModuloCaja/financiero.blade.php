88c64d532621a8e8537fa705e124cd567374562b
ivpsa <ivan_Vilugron@hotmail.es>
2017-10-10 22:13:30 -0400

    financiero correciones

--- resources/views/ModuloCaja/financiero.blade.php
+++ resources/views/ModuloCaja/financiero.blade.php
@@ -125,7 +125,7 @@
 
 
         <div class="container">
-        <a href="{{route('moduloCaja')}}"><button class="btn btn-primary btn-lg"> Volver</button></a>
+        <a href="javascript:history.back(1)"><button class="btn btn-primary btn-lg">Volver</button></a>
 
         </div>
         </a>
