efb78b60fb5d6f866942fa6559dd006843fefa45
cristophernic <cristophernic@live.cl>
2018-04-09 00:55:12 -0300

    prueba tablas

--- resources/views/ModuloCrm/resumen.blade.php
+++ resources/views/ModuloCrm/resumen.blade.php
@@ -69,36 +69,118 @@
                 </div>
             </div>
         </div>
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title" style="color:#FFF;">Resumen de oportunidades</h3>
-            </div>
-            <div class="panel-body">
+        <div class="panel-group" id="oportunidades" role="tablist">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title" style="color:#FFF;"><a role="button" data-toggle="collapse" data-parent="#oportunidades" href="#oportunidadesT">Resumen de oportunidades</a></h3>
+                </div>
+                <div id="oportunidadesT" class="panel-collapse collapse in" role="tabpanel">
+                    <div class="panel-body">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Proceso de negocio</th>
+                                    <th scope="col">Etapa</th>
+                                    <th scope="col">Cantidad</th>
+                                    <th scope="col">Esperado</th>
+                                    <th scope="col">Real</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row" colspan="6">(sin datos)</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title" style="color:#FFF;">Próximas actividades</h3>
-            </div>
-            <div class="panel-body">
+        <div class="panel-group" id="actividades" role="tablist">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title" style="color:#FFF;"><a role="button" data-toggle="collapse" data-parent="#actividades" href="#actividadesT">Próximas actividades</a></h3>
+                </div>
+                <div id="actividadesT" class="panel-collapse collapse in" role="tabpanel">
+                    <div class="panel-body">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Equipo</th>
+                                    <th scope="col">Responsable</th>
+                                    <th scope="col">Asunto</th>
+                                    <th scope="col">Actividad</th>
+                                    <th scope="col">Fecha inicio</th>
+                                    <th scope="col">Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row" colspan="7">(sin datos)</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title" style="color:#FFF;">Últimos candidatos</h3>
-                    </div>
-                    <div class="panel-body">
+                <div class="panel-group" id="candidatos" role="tablist">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title" style="color:#FFF;"><a role="button" data-toggle="collapse" data-parent="#candidatos" href="#candidatosT">Últimos candidatos</a></h3>
+                        </div>
+                        <div id="candidatosT" class="panel-collapse collapse in" role="tabpanel">
+                            <div class="panel-body">
+                            <table class="table table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col"></th>
+                                        <th scope="col">Indetificador</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Empresa</th>
+                                        <th scope="col">Procedencia</th>
+                                        <th scope="col">Fecha</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row" colspan="6">(sin datos)</th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
-                <div class="panel panel-primary"> 
-                    <div class="panel-heading">
-                        <h3 class="panel-title" style="color:#FFF;">Últimas cuentas</h3>
-                    </div>
-                    <div class="panel-body">
+                <div class="panel-group" id="cuentas" role="tablist">
+                    <div class="panel panel-primary"> 
+                        <div class="panel-heading">
+                            <h3 class="panel-title" style="color:#FFF;"><a role="button" data-toggle="collapse" data-parent="#cuentas" href="#cuentasT">Últimas cuentas</a></h3>
+                        </div>
+                        <div id="cuentasT" class="panel-collapse collapse in" role="tabpanel">
+                            <div class="panel-body">
+                                <thead>
+                                    <tr>
+                                        <th scope="col"></th>
+                                        <th scope="col">Indetificador</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Empresa</th>
+                                        <th scope="col">Propietario</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row" colspan="5">(sin datos)</th>
+                                    </tr>
+                                </tbody>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
