c20ed0ece4a7f1e7b49f46973c31e0708a3a15d8
cristophernic <cristophernic@live.cl>
2018-04-09 01:02:17 -0300

    prueba embudo

--- resources/views/ModuloCrm/resumen.blade.php
+++ resources/views/ModuloCrm/resumen.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Resumen</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/funnel.js"></script>
 </head>
 <body>
 @extends('layouts.app')
@@ -63,6 +65,7 @@
                                 <button type="button" class="btn btn-primary">Buscar</button>
                             </div>
                             <div class="col-md-6">
+                                <div id="funel"></div>
                             </div>
                         </div>
                     </div>
@@ -192,6 +195,44 @@
 
 
 @endsection
+<script>
 
+
+Highcharts.chart('funel', {
+    chart: {
+        type: 'funnel'
+    },
+    title: {
+        text: 'Sales funnel'
+    },
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b> ({point.y:,.0f})',
+                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                softConnector: true
+            },
+            center: ['40%', '50%'],
+            neckWidth: '30%',
+            neckHeight: '25%',
+            width: '80%'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    series: [{
+        name: 'Unique users',
+        data: [
+            ['Website visits', 15654],
+            ['Downloads', 4064],
+            ['Requested price list', 1987],
+            ['Invoice sent', 976],
+            ['Finalized', 846]
+        ]
+    }]
+});
+</script>
 </body>
 </html>
