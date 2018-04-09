3d1370302c19a3f6797a55838b064cc3dc70c882
cristophernic <cristophernic@live.cl>
2018-04-09 01:21:53 -0300

    revert

--- resources/views/ModuloCrm/resumen.blade.php
+++ resources/views/ModuloCrm/resumen.blade.php
@@ -199,41 +199,41 @@
 <script>
 
 $(document).ready(function(){
-    $("#funel").highcharts({
-        chart: {
-            type: 'funnel'
-        },
-        title: {
-            text: 'Sales funnel'
-        },
-        plotOptions: {
-            series: {
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b> ({point.y:,.0f})',
-                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
-                    softConnector: true
-                },
-                center: ['40%', '50%'],
-                neckWidth: '30%',
-                neckHeight: '25%',
-                width: '80%'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        series: [{
-            name: 'Unique users',
-            data: [
-                ['Website visits', 15654],
-                ['Downloads', 4064],
-                ['Requested price list', 1987],
-                ['Invoice sent', 976],
-                ['Finalized', 846]
-            ]
-        }]
-    });
+    Highcharts.chart('funel', {
+    chart: {
+        type: 'funnel'
+    },
+    title: {
+        text: 'Sales funnel'
+    },
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b> ({point.y:,.0f})',
+                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                softConnector: true
+            },
+            center: ['40%', '50%'],
+            neckWidth: '30%',
+            neckHeight: '25%',
+            width: '80%'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    series: [{
+        name: 'Unique users',
+        data: [
+            ['Website visits', 15654],
+            ['Downloads', 4064],
+            ['Requested price list', 1987],
+            ['Invoice sent', 976],
+            ['Finalized', 846]
+        ]
+    }]
+});
 });
 </script>
 </body>
