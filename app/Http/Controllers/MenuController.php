b76b1d51184f86682cd0400f5aab7f1911a8c05e
cristophernic <cristophernic@live.cl>
2018-04-12 10:29:02 -0300

    verify

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -8,10 +8,6 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     public function getMenus()
     {
