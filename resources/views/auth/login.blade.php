64eba9b83f4363a6cfe31eac5ef188b511107585
cristophernic <cristophernic@live.cl>
2018-04-16 10:54:44 -0300

    insertar login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,68 @@
                                     <img src="images/login_anfitrion.png" class="img-fluid" alt="Ejecutiva Bienvenida">
                                 </div>
                                 <div class="col-6">
-
+                                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                                        {{ csrf_field() }}
+                                        <div class="form-group{{ $errors->has('PRO_RUN') ? ' has-error' : '' }}">
+                                            <label for="rut" class="col-md-4 control-label">Empresa</label>
+                                            <div class="col-sm-10">
+                                                <select class="form-control" required autofocus>
+                                                    <option value="0">Seleccione </option>
+                                                    <option value="1">BIOGEST </option>
+                                                    <option value="2">TRENER </option>
+                                                    <option value="3">LOICA </option>
+                                                    <option value="4">KUTRALCO </option>
+                                                    <option value="5">IMPROTOUR </option>
+                                                </select>
+                                                @if ($errors->has('PRO_RUN'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('PRO_RUN') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="form-group{{ $errors->has('PRO_RUN') ? ' has-error' : '' }}">
+                                            <label for="rut" class="col-md-4 control-label">RUT</label>
+                                            <div class="col-sm-10">
+                                                <input id="PRO_RUN" type="text" class="form-control" name="PRO_RUN" value="{{ old('PRO_RUN') }}" required autofocus>
+                                                @if ($errors->has('PRO_RUN'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('PRO_RUN') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                            <label for="password" class="col-md-4 control-label">Contraseña</label>
+                                            <div class="col-sm-10">
+                                                <input id="password" type="password" class="form-control" name="password" required>
+                                                @if ($errors->has('password'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class=" col-sm-10">
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordarme
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-md-8 col-md-offset-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    Iniciar sesión
+                                                </button>
+                                                <!--<a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    ¿Olvidaste tu contraseña?
+                                                </a>-->
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
