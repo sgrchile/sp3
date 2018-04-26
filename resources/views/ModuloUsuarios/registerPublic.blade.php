9d4613cf4b67b19a99e57ad6ced49bc13a1352a7
cristophernic <cristophernic@live.cl>
2018-04-26 17:50:07 -0300

    try2

--- resources/views/ModuloUsuarios/registerPublic.blade.php
+++ resources/views/ModuloUsuarios/registerPublic.blade.php
@@ -256,7 +256,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function(){
-            $.get("https://plataforma.sgrchile.com/api/pais/all").done(function(data){
+            $.get("https://plataforma.sgrchile.com/api/pais").done(function(data){
                 if (data !== null){
                     if (Object.keys(data).length > 0 ){
                         $.each(data, function( index, value ){
