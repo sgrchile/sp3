272f366a5b947f4a4055aed0c269749826c8382e
cristophernic <cristophernic@live.cl>
2018-04-26 17:45:49 -0300

    json register

--- resources/views/ModuloUsuarios/registerPublic.blade.php
+++ resources/views/ModuloUsuarios/registerPublic.blade.php
@@ -51,22 +51,21 @@
                                                 <input type="date" class="form-control" id="fecha">
                                             </div>
                                             <div class="form-group col-12 col-md-6">
-                                                <label for="sexo">Sexo</label>
-                                                <select class="form-control" id="sexo">
-                                                    <option value="1">Hombre</option>
-                                                    <option value="2">Mujer</option>
-                                                    <option value="3">Indefinido</option>
+                                                <label for="genero">Género</label>
+                                                <select class="form-control" id="genero">
+                                                    <option value="1">Masculino</option>
+                                                    <option value="2">Femenino</option>
                                                 </select>
                                             </div>
                                             <div class="form-group col-12 col-md-6">
                                                 <label for="estado">Estado civil</label>
                                                 <select class="form-control" id="estado">
-                                                    <option value="1">Viudo</option>
+                                                    <option value="1">Viudo (a)</option>
                                                     <option value="2">Soltero (a)</option>
-                                                    <option value="3">Separado(a)</option>
-                                                    <option value="4">Divorciado(a)</option>
+                                                    <option value="3">Separado (a)</option>
+                                                    <option value="4">Divorciado (a)</option>
                                                     <option value="5">Convive</option>
-                                                    <option value="6">Casado(a)</option>
+                                                    <option value="6">Casado (a)</option>
                                                 </select>
                                             </div>
                                             <div class="form-group col-12 col-md-6">
@@ -129,8 +128,6 @@
                                             <div class="form-group">
                                                 <label for="pais">País</label>
                                                 <select class="form-control" id="pais">
-                                                    <option value="1">Otro</option>
-                                                    <option value="2">Chile</option>
                                                 </select>
                                             </div>
                                             <div class="form-group">
@@ -151,10 +148,6 @@
                                                     <option value="1">No corresponde</option>
                                                 </select>
                                             </div>
-                                            <div class="form-group">
-                                                <label for="referencia">Referencia</label>
-                                                <input type="text" class="form-control" id="referencia">
-                                            </div>
                                         </div>
                                         <div class="card-footer">
                                             <a href="#" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseTwo">Volver al paso 2</a>
@@ -169,12 +162,33 @@
                                         </div>
                                         <div class="card-body">
                                             <div class="form-group">
+                                                <label for="fechapago">Pago</label>
+                                                <select class="form-control" id="fechapago">
+                                                    <option value="1">Día</option>
+                                                    <option value="2">Semana</option>
+                                                    <option value="3">Quincena</option>
+                                                    <option value="4">Mes</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
                                                 <label for="fechapago">Fecha de Pago</label>
-                                                <input type="email" class="form-control" id="fechapago">
+                                                <select class="form-control" id="fechapago">
+                                                    <option value="1">25</option>
+                                                    <option value="2">26</option>
+                                                    <option value="3">27</option>
+                                                    <option value="4">28</option>
+                                                    <option value="5">29</option>
+                                                    <option value="6">30</option>
+                                                    <option value="7">1</option>
+                                                    <option value="8">2</option>
+                                                    <option value="9">3</option>
+                                                    <option value="10">4</option>
+                                                    <option value="11">5</option>
+                                                </select>
                                             </div>
                                             <div class="form-group">
                                                 <label for="ncuenta">Número de cuenta</label>
-                                                <input type="text" class="form-control" id="ncuenta">
+                                                <input type="number" class="form-control" id="ncuenta">
                                             </div>
                                             <div class="form-group">
                                                 <label for="banco">Banco</label>
@@ -240,5 +254,58 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+            $.get("https://plataforma.sgrchile.com/api/pais/all").done(function(data){
+                if (data !== null){
+                    if (Object.keys(data).length > 0 ){
+                        $.each(data, function( index, value ){
+                            let option = "<option value='"+ value.PAI_COD + "'>" + value.PAI_DESC+ "</option>";
+                            $("#pais").append(option);
+                        });
+                    }
+                }
+            });
+            $.get("https://plataforma.sgrchile.com/api/region").done(function(data){
+                if (data !== null){
+                    if (Object.keys(data).length > 0 ){
+                        $.each(data, function( index, value ){
+                            let option = "<option value='"+ value.REG_COD + "'>" + value.REG_DESC+ "</option>";
+                            $("#region").append(option);
+                        });
+                    }
+                }
+            });
+            $("#region").on("change", function(){
+                let region = $(this).val();
+
+                $.get("https://plataforma.sgrchile.com/api/provincia/" + region).done(function(data){
+                    if (data !== null){
+                        if (Object.keys(data).length > 0 ){
+                            $.each(data, function( index, value ){
+                                let option = "<option value='"+ value.PV_COD + "'>" + value.PV_DESC+ "</option>";
+                                $("#provincia").append(option);
+                            });
+                        }
+                    }
+                });
+            });
+
+            $("#provincia").on("change", function(){
+                let provincia = $(this).val();
+
+                $.get("https://plataforma.sgrchile.com/api/ciudad/" + provincia).done(function(data){
+                    if (data !== null){
+                        if (Object.keys(data).length > 0 ){
+                            $.each(data, function( index, value ){
+                                let option = "<option value='"+ value.CIU_COD + "'>" + value.CIU_DESC+ "</option>";
+                                $("#ciudad").append(option);
+                            });
+                        }
+                    }
+                });
+            });
+        });
+    </script>
   </body>
 </html>
