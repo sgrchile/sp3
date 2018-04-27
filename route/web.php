8e46fb65fdf4ae9d14c255d10d4405ee9bb0abd3
cristophernic <cristophernic@live.cl>
2018-04-27 17:30:46 -0300

    corregir

--- route/web.php
+++ route/web.php
@@ -15,7 +15,7 @@ Route::get('/',function(){
        return view('auth/login');
    })->name('login');
 
-Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
    // Registrar
 Route::get('/registrar', function () {
     return view('ModuloUsuarios/registerPublic');
