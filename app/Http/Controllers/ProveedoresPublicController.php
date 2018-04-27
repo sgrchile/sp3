67732aa1bbbc5f4a8736926d5eb4ffa261b86d3e
cristophernic <cristophernic@live.cl>
2018-04-27 16:03:02 -0300

    objeto

--- app/Http/Controllers/ProveedoresPublicController.php
+++ app/Http/Controllers/ProveedoresPublicController.php
@@ -31,11 +31,12 @@ class ProveedoresPublicController extends Controller
         $createProveedorPersona = $this->createProveedorPersona($request->all());
 
         if (! $createProveedorPersona) {
-            $respuesta = '{"respuesta": "Hubo un problema al crear el proveedor persona."}';
+            $respuesta = new stdClass;
+            $respuesta->respuesta = "Hubo un problema al crear el proveedor persona.";
             return response()->json($respuesta);
         }
-
-        $respuesta = '{"respuesta": "El proveedor ha sido creado exitosamente."}';
+        $respuesta = new stdClass;
+        $respuesta->respuesta = "El proveedor ha sido creado exitosamente.";
         return response()->json($respuesta);
     }
 
