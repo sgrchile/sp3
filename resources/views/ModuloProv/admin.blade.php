e44ed1735ed4426fbfe854e5c54dd52a67393e32
cristophernic <cristophernic@live.cl>
2018-04-27 20:24:43 -0300

    prueba

--- resources/views/ModuloProv/admin.blade.php
+++ resources/views/ModuloProv/admin.blade.php
@@ -45,6 +45,7 @@
         </div>
     </div>
 @endsection
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function(){
         $("#tabla\\.proveedores tr").on('click', function() {
