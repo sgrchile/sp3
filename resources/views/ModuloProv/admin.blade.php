17354f9887a1623787f034c8b5c2dfdca4e08911
cristophernic <cristophernic@live.cl>
2018-04-27 19:19:45 -0300

    base uno

--- resources/views/ModuloProv/admin.blade.php
+++ resources/views/ModuloProv/admin.blade.php
@@ -2,11 +2,49 @@
 @section('content')
     <div class="container" align="center">
         <div class="row">
-            <div class="col-xs-12 col-md-12 col-sm-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Administración de Módulo Proveedores</div>
+            <div class="col-xs-8 col-md-8 col-sm-8">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Administración de Módulo Proveedores</h3>
+                    </div>
                     <div class="panel-body">
-                        Panel content
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li role="presentation" class="active"><a href="#alta" aria-controls="alta" role="tab" data-toggle="tab">Alta de Proveedor</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id="alta">
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>First Name</th>
+                                            <th>Last Name</th>
+                                            <th>Username</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
