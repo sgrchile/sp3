6340cf52e1c742a0e810826ea2806da0741f77cd
cristian <cristianG>
2018-05-04 17:46:22 -0300

    act btn registro

--- resources/views/ModuloCrm/registro.blade.php
+++ resources/views/ModuloCrm/registro.blade.php
@@ -20,10 +20,10 @@
 
                             <table class="table-condensed">
                                 <tr>
-                                    <td><a href="{{route('regCliente')}}"><button class='btn btn-primary btn-lg'   style="width:185px; margin-bottom: 15px;">REGISTRAR CLIENTES</button></a></td>
+                                    <td><a href="{{route('regCliente')}}"><button class='btn btn-primary btn-lg'   style="width:250px; margin-bottom: 15px;">REGISTRAR CLIENTES</button></a></td>
                                 </tr>
                                 <tr>
-                                    <td><a href="{{route('regProspecto')}}"><button class='btn btn-primary btn-lg' style="width:185px; margin-bottom: 15px;"> REGISTRAR PROSPECTO</button></a></td>
+                                    <td><a href="{{route('regProspecto')}}"><button class='btn btn-primary btn-lg' style="width:250px; margin-bottom: 15px;"> REGISTRAR PROSPECTO</button></a></td>
                                 </tr>
                             </table>
 
